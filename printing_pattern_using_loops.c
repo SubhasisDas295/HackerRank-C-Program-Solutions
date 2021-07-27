06075933dbe3dda478b74352536352ecf178d83b
Subhasis Das <69499562+SubhasisDas295@users.noreply.github.com>
2021-07-27 16:42:43 +0530

    Add files via upload

---
+++ printing_pattern_using_loops.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	int len=n*2-1,i,j,min;
+    for(i=0;i<len;i++)
+    {
+     for(j=0;j<len;j++)
+     {
+         min=i<j?i:j;
+         min=min<len-i?min:len-i-1;
+         min=min<len-j-1?min:len-j-1;
+         printf("%d ",n-min);
+     }
+     printf("\n");
+    } 
+    return 0;
+}
